6cd26686e5ed02be03c490c65f6720dad20e936f
sudipadk10 <adhikarisudip869@gmail.com>
2024-12-16 21:13:28 +0545

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+let name = "John" //stying
+let age = 30 //number
+let isfull = false //boolean
+
+/*
+null
+undefined
+
+*/
+
+console.log(typeof 100)
+console.log(typeof "John")
+console.log(typeof true)
+console.log(typeof undefined) //undefined
+console.log(typeof null)  //object
---
+++ variable.js
@@ -0,0 +1,19 @@
+const id = 123
+var name = "John"
+let age = 25
+address = "Biratnagar"
+let email
+// id=90
+
+name = "Ram"
+age = 30
+address = "Ktm"
+
+console.table([id,name,age,address,email]);
+
+/*  
+let and var are the same but...
+Not recommended to use var
+var is function scoped and hoisted to the top of the script.
+It can be reassigned but not redeclared.
+*/
