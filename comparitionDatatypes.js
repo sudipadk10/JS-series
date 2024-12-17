de336fb0e8ab0d87c49e05d556c80561adb94f5b
sudipadk10 <adhikarisudip869@gmail.com>
2024-12-17 21:13:00 +0545

    comparision of datatypes

---
+++ comparitionDatatypes.js
@@ -0,0 +1,28 @@
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2!= 1);
+// console.log(2!== 1);
+// console.log(true == 1);
+
+// console.log(true < 2); //true
+// console.log("2" < 3); //true
+// console.log("aura" > 1000000000); //false
+// console.log("apple" < "anana"); //true
+// console.log("naman" == "naman"); //true
+// console.log(2.000 == 2); //true
+
+// console.log(null > 0); // false
+// console.log(null == 0); //false ->equality check
+// console.log(null >= 0); //true ->comparison check
+
+// === strict check (checks for datatype also)
+
+// console.log("2" == 2); //true
+// console.log("2" === 2); //false
+// console.log(Number(null) === 0) //true
+// console.log("3" === "3"); //true
+
+
+
+
