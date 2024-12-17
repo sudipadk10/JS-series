84ebacd6d4eb4a2a722a5e1234af2bd887418c0a
sudipadk10 <adhikarisudip869@gmail.com>
2024-12-17 20:13:38 +0545

    conversion operators

---
+++ conversionOperation.js
@@ -0,0 +1,28 @@
+// let s1 = "hello"
+// let s2 = "world"
+// let result = s1 + s2
+// console.log(result);
+
+// console.log("2 power 5 is " + 2**5); // 32
+// console.log("2 added to 5 is " + 2 + 5); // 25
+// console.log("Hello" + " "+ "World"); // Hello World
+// console.log("3" + 2 + 1); //321
+// console.log(3 + 2 + "1") //51
+// console.log(3 + "2" + 1 - 2) //319
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+
+let number = 10
+++number 
+// number++
+console.log(number) // 11
+
+
+/*diff between prefix and postfix operator 
+prefix increment (++number)
+The value of number is incremented first, then it is used.
+
+postfix increment (number++)
+The value of number is used, then it is incremented.
+*/
