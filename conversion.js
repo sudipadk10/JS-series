4dfe10bbfa526b937a545f5a957f8faebd72f10e
sudipadk10 <adhikarisudip869@gmail.com>
2024-12-17 12:19:01 +0545

    convertion of datatypes

---
+++ conversion.js
@@ -0,0 +1,44 @@
+// let score = null
+// let marks = "69%"
+
+// NumScore=Number(score)
+// console.log(NumScore)
+// console.log(typeof score) // string
+// console.log(typeof marks) // string
+
+// //converting string into number
+// NumMarks=Number(marks)
+
+// console.log(typeof NumScore) // number
+// console.log(typeof NumMarks) // number'
+// console.log(NumMarks) //NaN
+
+/* converting into number
+"33" => 33
+"33abd" => NaN (Not a number)
+null => 0
+undefined => NaN
+true => 1
+*/
+
+
+
+
+// //conveting into Boolean
+// let num = ""
+// let isTrue = Boolean(num)
+// console.log(typeof isTrue) //boolean
+// console.log(isTrue) //true
+
+// /*
+// 0.0001 ; 1 ; 12 ; "string" ; "false" => true
+// 0 ; "" ;null ; undefined =>false
+// */
+
+
+//conversion into string
+
+let value = 0
+let stringValue = String(value)
+console.log(stringValue);
+console.log(typeof stringValue);
