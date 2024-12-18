e61de474b902876ae20338d2f073f670c32f37ec
sudipadk10 <adhikarisudip869@gmail.com>
2024-12-18 22:24:00 +0545

    Strings in javascript

---
+++ strings.js
@@ -0,0 +1,29 @@
+ const language = "Javascript"
+// let mood = "happy"
+// console.log(`Hello I am currently learning ${language} and I am ${mood}.`);
+
+
+//Object 
+const Name = new String("Ramesh")
+// console.log(Name[3]); //e
+// console.log(Name.length); //6
+// console.log(Name.toUpperCase()); // RAMESH  ->It will not change the original value.
+// console.log(Name.charAt(2)); //m
+// console.log(Name.indexOf('e'))  //3 
+// console.log(Name.substring(1,3)); //am
+// console.log(Name.slice(-5,3)); //am  -> counts from behind which substring does not allow 
+// console.log(Name.endsWith("h")); //true
+
+
+const newstring = new String("   aayo aayo    ");
+// console.log(newstring.trim()); //aayo aayo  -> removes spaces from beggining and ending of a string .
+
+const email = new String("adhikarisudip10%69gmail.com")
+// console.log(email.replace('%69','@')); 
+// console.log(email.includes("@")); // false 
+
+const url = new String("blogs.adhikarisudip10.com.np")
+arrayUrl = url.split(".")  // It splits on the basis of given divider and make array.
+console.log(arrayUrl); 
+
+
