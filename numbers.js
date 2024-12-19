368b8222a6b483839c32d0f03add9258528183e2
sudipadk10 <adhikarisudip869@gmail.com>
2024-12-19 21:54:18 +0545

    Numbers in Javascript

---
+++ numbers.js
@@ -0,0 +1,11 @@
+const score = new Number(257)
+// console.log(score);
+// console.log(score.toString().length) //After converting into string we can use properties of string 
+
+const tax = 273.51261
+// console.log(tax.toFixed(3)); //273.513
+// console.log(tax.toPrecision(5)); //273.51
+
+const salary = 100000000
+console.log(salary.toLocaleString('en-IN'));
+
