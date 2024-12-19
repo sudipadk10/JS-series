c26ab70cfbe1feaff4629977d52e1b76bbde25bb
sudipadk10 <adhikarisudip869@gmail.com>
2024-12-19 22:18:19 +0545

    Math in js

---
+++ maths.js
@@ -0,0 +1,17 @@
+// console.log(Math);
+// console.log(Math.abs(-5)); // 5
+// console.log(Math.round(5.49)); //5
+// console.log(Math.pow(4,2)); //16
+
+
+
+// console.log(Math.random()); // generates floot value between 0 and 1
+// console.log(Math.random()*10);  // generates floot value between 0 and 1
+// console.log(Math.random()*10+1); // generates floot value between 1 and 11 
+// console.log(Math.floor(Math.random()*10+1)); //generates integer value from 1 to 10 
+
+const min = 10
+const max= 20
+console.log(Math.floor(Math.random()*(max-min)+1+min)); //generates integer value between min and max
+
+
