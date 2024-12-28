15eb269621ae8dff066d240b6d226d81bffd3406
sudipadk10 <adhikarisudip869@gmail.com>
2024-12-28 21:11:29 +0545

    control flow

---
+++ controlFlow.js
@@ -0,0 +1,26 @@
+// let score = 70;
+
+// if(score > 80){
+//     console.log("very Good ");
+    
+// }
+// else if(score > 50)
+// {
+//     console.log("Good ");
+// }
+// else{
+//     console.log("Not Good ");
+// }
+
+
+let a = 20
+// if (a>10) console.log("a>10"),console.log("second line ");   
+;
+
+
+//nullish Coalising operator ??
+let name;
+// name = "sudip" ?? "SPAI"; //sudip
+// name = null ?? "SPAI" ; //SPAI
+name = undefined ?? "SPAI" ; //SPAI
+// console.log(name); 
