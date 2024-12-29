2f033d73a1c4022242591db3abd48cbf33b4b297
sudipadk10 <adhikarisudip869@gmail.com>
2024-12-29 20:12:00 +0545

    while loop

--- for_loop.js
+++ for_loop.js
@@ -5,10 +5,34 @@
 // 1 2 3 4
 // 1 2 3 4 5
 
-for(let i=1;i<=5;i++){
-    let output = "";
-    for(let j=1;j<=i;j++){
-        output = output + " "+j;
+// for(let i=1;i<=5;i++){
+//     let output = "";
+//     for(let j=1;j<=i;j++){
+//         output = output + " "+j;
+//     }
+//     console.log(output);
+// }
+
+
+// break and continue 
+// break stops the loop and continue just skips the current iteration and moves to the next one 
+
+// for (let i=1;i<=5;i++){
+//     if(i==3){
+//         console.log("3 detected");
+        
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+for (let i=1;i<=5;i++){
+    if(i==3){
+        console.log("3 detected");
+        
+        continue;
     }
-    console.log(output);
+    console.log(i);
 }
+
---
+++ while_loop.js
@@ -0,0 +1,14 @@
+
+// let i = 1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+let i = 0;
+const  arr = new Array("sirjan","gandu","ho")
+
+do{
+    console.log(arr[i]);
+    i++;
+}while(i<arr.length);
