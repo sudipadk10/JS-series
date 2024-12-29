6beae25561b557662c3e3e548978f41fedeb5a69
sudipadk10 <adhikarisudip869@gmail.com>
2024-12-29 20:00:31 +0545

    for loop

---
+++ for_loop.js
@@ -0,0 +1,14 @@
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+for(let i=1;i<=5;i++){
+    let output = "";
+    for(let j=1;j<=i;j++){
+        output = output + " "+j;
+    }
+    console.log(output);
+}
